a4bd3419fd6dab9a89afe9faf57314dc7fab257d
VasylArtymovych <art777vasss@gmail.com>
2023-03-04 12:45:40 +0200

    remove comments

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,10 +4,3 @@ export const inputStyle = (error: boolean, title: string) => {
   }
   return '1px solid black';
 };
-
-// export const descrInputStyle = (error: boolean, text: string) => {
-//   if (error && text.length === 0) {
-//     return '1px solid red';
-//   }
-//   return '1px solid black';
-// };
